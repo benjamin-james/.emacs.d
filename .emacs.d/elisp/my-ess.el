39da25dcf62aaa827bee7a930d3ef6d05ddcc31e
Benjamin James <benjames@mit.edu>
2024-08-16 10:24:44 -0400

    added some polymode/ESS, hooked tree-sitter, fixed kl1-4 servers
    NCSA Nationalist Plame GIGN Aid H&K DJC grey data HAHO Matamoros Brush
    fire National laboratory SADF World Health Organization China

--- .emacs.d/elisp/my-ess.el
+++ .emacs.d/elisp/my-ess.el
@@ -1,10 +1,15 @@
 
 					;https://github.com/MilesMcBain/esscss
 
-(use-package ess
-  
-  )
+(use-package ess)
 
-
+(use-package polymode
+  :straight t
+  :mode (("\\.Rmd\\'" . poly-markdown+r-mode))
+  :config
+  (use-package poly-R
+    :straight t)
+  (use-package poly-markdown
+    :straight t))
 (use-package ess-view
   :after ess)
--- .emacs.d/elisp/my-tree-sitter.el
+++ .emacs.d/elisp/my-tree-sitter.el
@@ -2,10 +2,10 @@
 ;; Install and configure tree-sitter for syntax highlighting
 (use-package tree-sitter
   :ensure t
+  :hook (after-init . global-tree-sitter-mode) 
   :config
   (use-package tree-sitter-langs
     :ensure t)
-  (global-tree-sitter-mode) 
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
 (provide 'my-tree-sitter)
